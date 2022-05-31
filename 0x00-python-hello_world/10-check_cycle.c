4c626d06b56bbec8732e93539f7cae9b860192a2
bmokase3093 <bmokase3093@gmail.com>
2022-05-30 19:35:38 -0700

    Check for singly linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,69 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
-  * check_cycle - checks if a singly linked list has a cycle.
-  * @list: pointer to first element.
-  *
-  * Return: 1 if cycle is found 0 if not.
-  */
+ * _realloc - Reallocates a memory block
+ * @ptr: The pointer to the previous memory block
+ * @old_size: The size of the old memory block
+ * @new_size: The size of the new memory block
+ *
+ * Return: The pointer to the new memory block otherwise NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int min_size = old_size < new_size ? old_size : new_size;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr != NULL)
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		new_ptr = malloc(new_size);
+		if (new_ptr != NULL)
+		{
+			for (i = 0; i < min_size; i++)
+				*((char *)new_ptr + i) = *((char *)ptr + i);
+			free(ptr);
+			return (new_ptr);
+		}
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+}
+
+
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: list head
+ * Return: 0 if no, 1 if yes
+ */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *current, *next_node;
+	listint_t *fast, *slow = list;
 
 	if (list == NULL)
 		return (0);
 
-	current = list;
-	next_node = list;
-
-	while (current && next_node && next_node->next)
+	fast = list->next;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
-		current = current->next;
-		next_node = next_node->next->next;
-		if (current == next_node)
+		if (slow == fast)
 			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
 	}
-	if (current != next_node)
-		return (0);
-	return (1);
+	return (0);
 }
