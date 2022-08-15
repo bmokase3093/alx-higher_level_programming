da188b063aaf8ff0d87c92a9225ebaff8c486798
bmokase3093 <bmokase3093@gmail.com>
2022-08-15 07:17:01 -0700

    fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,7 @@
 -- Each record should display tv_shows.title, tv_genres.name
 -- Results must be sorted in ascending order by show title
 -- You can only use one SELECT statement
+
 SELECT s.title, g.name
 FROM tv_shows s
 LEFT JOIN tv_show_genres m ON s.id = m.show_id
