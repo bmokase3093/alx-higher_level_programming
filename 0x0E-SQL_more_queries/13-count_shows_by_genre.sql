708f40bfbe44e3448f4b2a11934ccf0a46269ab2
bmokase3093 <bmokase3093@gmail.com>
2022-08-15 07:15:50 -0700

    fix count shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,9 @@
--- List all genres from 'hbtn_0d_tvshows' and display number of shows linked to each
--- Each record should display tv_genres.name, number of shows
--- Don't display a genre that doesn't have any shows linked
--- Results must be sorted descending order by number of shows linked
--- Can only use one SELECT statement
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_shows DESC;
+-- list all genres from hbtn_0d_tvshows and display number of shows linked to each
+-- record is in the form <tv show genre> - <number of shows linked to it>
+-- sorted in desc order of number of shows linked
+
+SELECT tv_g.name AS genre, COUNT(show_id) AS number_of_shows FROM tv_genres tv_g
+INNER JOIN tv_show_genres tvs_g
+ON tv_g.id = tvs_g.genre_id
+GROUP BY tvs_g.genre_id
+ORDER BY number_of_shows DESC;
